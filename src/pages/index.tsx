60d450e67c73ae60bf8658bbaa93751c39668cbe
tsukuneA1 <tkschr1125@gmail.com>
2025-04-03 00:39:23 +0900

    pokemon データ　読み込みテスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,11 @@ import { MainLayout } from "@/layouts/main/main-layout";
 import { useEffect, useState } from "react";
 import { Move } from "@/types/move";
 import { MoveCard } from "@/components/move-card";
+import { Pokemon } from "@/types/pokemon";
 
 export default function Home() {
   const [moves, setMoves] = useState<Move[]>([]);
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
   useEffect(() => {
     fetch("/api/moves")
@@ -12,10 +14,25 @@ export default function Home() {
       .then((data) => setMoves(data))
       .catch((error) => console.error('Error fetching moves:', error));
   }, []);
+
+  useEffect(() => {
+    fetch("/api/pokemons")
+      .then((res) => res.json())
+      .then((data) => setPokemons(data))
+      .catch((error) => console.error('Error fetching pokemons:', error));
+  }, []);
+
   return (
     <MainLayout>
       <h2>hello world</h2>
       <ol>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>
+            <div>{pokemon.name}</div>
+          </li>
+        ))}
+      </ol>
+      <ol className="ml-5">
         {moves.map((move) => (
           <li key={move.id}>
             <MoveCard move={move} />
