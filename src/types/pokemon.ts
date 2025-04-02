08cbb03132e30d146514624beff23e5a4885d5cf
tsukuneA1 <tkschr1125@gmail.com>
2025-04-03 00:39:42 +0900

    type 追加

---
+++ src/types/pokemon.ts
@@ -0,0 +1,16 @@
+export type Pokemon = {
+    id: number;
+    name: string;
+    type1: string;
+    type2: string;
+    hp: number;
+    attack: number;
+    defense: number;
+    spAttack: number;
+    spDefense: number;
+    speed: number;
+    weight: number;
+    ability1: string;
+    ability2: string;
+    imgSrc: string;
+}
