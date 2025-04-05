a5838ce839967e840ee7d5cf43a3707c62a41a19
tsukuneA1 <tkschr1125@gmail.com>
2025-04-05 10:55:45 +0900

    データ取得チェックのためのフロント変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,23 @@
 import { MainLayout } from "@/layouts/main/main-layout";
 import { useEffect, useState } from "react";
-import { Move } from "@/types/move";
-import { MoveCard } from "@/components/move-card";
-import { Pokemon } from "@/types/pokemon";
-import Image from "next/image";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { FactoryPokemon } from "@/types/factoryPokemon";
+import { PokemonCard } from "@/components/pokemon-card";
 
 export default function Home() {
-  const [moves, setMoves] = useState<Move[]>([]);
-  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-
+  const [factoryPokemons, setFactoryPokemons] = useState<FactoryPokemon[]>([]);
   useEffect(() => {
-    fetch("/api/moves")
+    fetch("/api/factory_pokemon")
       .then((res) => res.json())
-      .then((data) => setMoves(data))
-      .catch((error) => console.error('Error fetching moves:', error));
-  }, []);
-
-  useEffect(() => {
-    fetch("/api/pokemons")
-      .then((res) => res.json())
-      .then((data) => setPokemons(data))
-      .catch((error) => console.error('Error fetching pokemons:', error));
+      .then((data) => setFactoryPokemons(data))
+      .catch((error) => console.error('Error fetching factory pokemons:', error));
   }, []);
 
   return (
     <MainLayout>
-      <h2>hello world</h2>
-      <ol>
-        {pokemons.map((pokemon) => (
-          <li key={pokemon.id}>
-            <Avatar className="border-2">
-              <AvatarImage src={pokemon.imageSrc} />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </li>
-        ))}
-      </ol>
       <ol className="ml-5">
-        {moves.map((move) => (
-          <li key={move.id}>
-            <MoveCard move={move} />
+        {factoryPokemons.slice(930, 940).map((factoryPokemon) => (
+          <li key={factoryPokemon.id}>
+             <PokemonCard pokemon={factoryPokemon} />
           </li>
         ))}
       </ol>
