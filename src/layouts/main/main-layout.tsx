896fa96469f4b77b6cb1d9aeb5c7ebde165d3593
tsukuneA1 <tkschr1125@gmail.com>
2025-04-10 12:03:25 +0900

    add sidebar

--- src/layouts/main/main-layout.tsx
+++ src/layouts/main/main-layout.tsx
@@ -1,14 +1,20 @@
+import { AppSidebar } from '@/components/app-sidebar';
 import { MainFooter } from '@/components/layout/main/main-footer';
 import { MainHeader } from '@/components/layout/main/main-header';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 
 export const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex min-h-screen flex-col">
-      <MainHeader />
-      <main className="flex flex-col flex-grow justify-center py-4 mx-5">
-        {children}
-      </main>
-      <MainFooter />
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <div className="flex min-h-screen flex-col">
+        <MainHeader />
+        <main className="flex flex-col flex-grow justify-center py-4 mx-5">
+          <SidebarTrigger />
+          {children}
+        </main>
+        <MainFooter />
+      </div>
+    </SidebarProvider>
   );
 };
