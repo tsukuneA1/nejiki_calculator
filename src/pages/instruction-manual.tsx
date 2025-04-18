d543cb3d514b8380c50692a2f2585a683d5445c3
tsukuneA1 <tkschr1125@gmail.com>
2025-04-18 15:38:58 +0900

    instruction-manual by readme

--- src/pages/instruction-manual.tsx
+++ src/pages/instruction-manual.tsx
@@ -1,37 +1,37 @@
 import { SubLayout } from '@/layouts/sub/sub-layout';
-
+import fs from 'fs';
+import path from 'path';
 import ReactMarkdown from 'react-markdown';
 
-export default function InstructionManual() {
-  const content = `# 使用説明書
-  ## はじめに
-  このツールは、ポケモンのダメージ計算を行うためのものです。ポケモンの技や特性、持ち物などを設定し、ダメージを計算することができます。
-  ## 使用方法
-  1. ポケモンを選択します。
-  2. 技を選択します。
-  3. 特性を選択します。
-  4. 持ち物を選択します。
-  5. レベルを選択します。
-  6. ダメージを計算します。
-  ## 注意事項 `;
-  
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'README.md');
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+
+  return {
+    props: {
+      markdown: fileContent,
+    },
+  };
+}
+
+export default function InstructionManual({ markdown }: { markdown: string }) {
   return (
     <SubLayout>
       <div className='prose min-h-80 w-full max-w-none rounded-lg border-2 border-gray-300 p-10'>
       <ReactMarkdown
         components={{
           h1: ({ node, ...props }) => (
-            <h1 {...props} className="flex items-center pb-2" />
+            <h1 {...props} className="flex items-center pb-2 text-3xl font-bold" />
           ),
           h2: ({ node, ...props }) => (
-            <h2 {...props} className="border-gray-300 border-b pb-1" />
+            <h2 {...props} className="border-gray-300 border-b pb-1 text-xl font-bold" />
           ),
           a: ({ node, ...props }) => (
             <a {...props} className="text-blue-500 hover:text-blue-700" />
           ),
         }}
       >
-        {content}
+        {markdown}
       </ReactMarkdown>
       </div>
       
