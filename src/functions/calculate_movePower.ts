661e7e7f276cdb22ea06cec36619395dd6fe2e99
tsukuneA1 <tkschr1125@gmail.com>
2025-05-14 01:21:18 +0900

    typescript型安全で素晴らしいけどビルド落ちるのはいらいらする

--- src/functions/calculate_movePower.ts
+++ src/functions/calculate_movePower.ts
@@ -2,15 +2,23 @@ import { typeStrengthen } from "@/constants/items";
 import type { Attacker } from "@/types/attacker";
 
 export const calculateMovePower = (attacker: Attacker) => {
-	let movePower = attacker.move?.power!;
-	movePower *= typeStrengthen({
-		type: attacker.move?.type!,
-		item: attacker.item!,
-	});
+	const move = attacker.move;
+	const item = attacker.item;
 
-	if (attacker.ability === "テクニシャン" && attacker.move?.power! <= 60) {
+	if (!move || !item) {
+		throw new Error("move または item が未定義です");
+	}
+
+	let movePower = move.power!;
+
+	if (attacker.ability === "テクニシャン" && movePower <= 60) {
 		movePower *= 1.5;
 	}
+
+	movePower *= typeStrengthen({
+		type: move.type,
+		item,
+	});
 
 	return movePower;
 };
