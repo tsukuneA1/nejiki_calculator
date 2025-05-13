9c88eb416a1b9be885d6cb48c22a9b0c6743297b
tsukuneA1 <tkschr1125@gmail.com>
2025-05-14 01:18:05 +0900

    fix error

--- src/components/type-badge.tsx
+++ src/components/type-badge.tsx
@@ -1,7 +1,4 @@
 import { TypeItems } from "@/constants/types";
-import { AvatarImage } from "@radix-ui/react-avatar";
-import { Avatar, AvatarFallback } from "./ui/avatar";
-import { Badge } from "./ui/badge";
 
 export const TypeBadge = ({ type }: { type: string }) => {
 	const typeItem = TypeItems.find((item) => item.type === type);
--- src/functions/calculate_movePower.ts
+++ src/functions/calculate_movePower.ts
@@ -4,7 +4,7 @@ import type { Attacker } from "@/types/attacker";
 export const calculateMovePower = (attacker: Attacker) => {
 	let movePower = attacker.move?.power!;
 	movePower *= typeStrengthen({
-		type: attacker.move?.type,
+		type: attacker.move?.type!,
 		item: attacker.item!,
 	});
 
