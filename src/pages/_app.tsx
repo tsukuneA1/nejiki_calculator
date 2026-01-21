5120d10d1976d9a2baf9997f46bf4936050d7e88
tsukuneA1 <tkschr1125@gmail.com>
2026-01-21 23:05:28 +0900

    google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Provider } from "react-redux";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -33,6 +34,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-58WG1FB0JL"/>
     </Provider>
   );
 }
