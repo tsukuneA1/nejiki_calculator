f026f655de196e7c10f15a35d3955ea82bfaf835
tsukuneA1 <tkschr1125@gmail.com>
2026-01-30 08:57:18 +0900

    fix:image cache and optimize

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    formats: ["image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
+    minimumCacheTTL: 31536000, // 1年間キャッシュ
+  },
 };
 
 export default nextConfig;
