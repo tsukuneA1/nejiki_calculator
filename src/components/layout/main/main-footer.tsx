a3cb8b69b8d9bec7e96059c7ef5f9383f17235ed
tsukuneA1 <tkschr1125@gmail.com>
2026-07-24 08:06:41 +0900

    ダメージ結果バーの最大幅を縮小

--- src/components/layout/main/main-footer.tsx
+++ src/components/layout/main/main-footer.tsx
@@ -2,7 +2,7 @@ import { Damage } from "@/components/domain/damage/damage";
 
 export const MainFooter = () => {
   return (
-    <footer className="fixed bottom-2 left-1/2 z-50 w-[calc(100%_-_1rem)] max-w-6xl -translate-x-1/2 rounded-lg border-2 border-gray-200 bg-white px-5 py-5 shadow-lg dark:border-slate-700 dark:bg-slate-900 sm:bottom-4 sm:w-[calc(100%_-_2.5rem)]">
+    <footer className="fixed bottom-2 left-1/2 z-50 w-[calc(100%_-_1rem)] max-w-5xl -translate-x-1/2 rounded-lg border-2 border-gray-200 bg-white px-5 py-5 shadow-lg dark:border-slate-700 dark:bg-slate-900 sm:bottom-4 sm:w-[calc(100%_-_2.5rem)]">
       <Damage />
     </footer>
   );
